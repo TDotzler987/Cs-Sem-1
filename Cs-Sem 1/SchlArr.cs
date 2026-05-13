764a075ab8747041bb290b1b85431cc0af74f528
TDotzler987 <thomasdotzler321@gmail.com>
2026-05-13 13:59:15 +0200

    SchlArr

--- Cs-Sem 1/SchlArr.cs
+++ Cs-Sem 1/SchlArr.cs
@@ -20,21 +20,17 @@ namespace Cs_Sem_1
             //Programm, das jedem Spieler aus strArray 6 Zufallszahlen gibt und das lottoZahlen-Array mit 6 Werten füllt
             Console.WriteLine("Bitte nennen Sie einen der folgenden Namen:");
             Console.WriteLine("Hans, Peter, Sophia, Stefan, Jules, Sky, Petronas, Georg, Amelie, Sebastian");
-            string name = Console.ReadLine();
+            string name = Console.ReadLine().Trim();
             bool nameGefunden = Array.Contains(name, StringComparer.OrdinalIgnoreCase);
             if (nameGefunden ) 
                 
                 {
-                    Console.WriteLine(($"Hallo {name} ihre Lottozahlen sind " + rnd.Next(0,50)+" , "+ rnd.Next(0, 50) + " , " + rnd.Next(0, 50) + " , " + rnd.Next(0, 50) + " , " + rnd.Next(0, 50) + " , " + rnd.Next(0, 50)));
-
+                    Console.WriteLine($"Hallo {name} ihre Lottozahlen sind " + rnd.Next(0,50)+" , "+ rnd.Next(0, 50) + " , " + rnd.Next(0, 50) + " , " + rnd.Next(0, 50) + " , " + rnd.Next(0, 50) + " , " + rnd.Next(0, 50));
                 }
-            
             else
-            {
-                Console.WriteLine("falsche Eingabe");
-            }
-
-
+                {
+                    Console.WriteLine("falsche Eingabe");
+                }
         }
     }
 }
